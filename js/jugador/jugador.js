5ae94ed3d68bc3a13e0580bda30c312ff6947f9c
erickpaul-a11y <erick.paul@escola.pr.gov.br>
2026-08-01 16:05:41 -0300

    Update jugador.js

--- js/jugador/jugador.js
+++ js/jugador/jugador.js
@@ -1,78 +1,73 @@
 class Jugador {
 
-constructor(){
+    constructor(){
 
-    this.x = 0;
-    this.z = 0;
-    this.y = 2;
+        this.x = 0;
+        this.z = 0;
+        this.y = 2;
 
-    this.hp = 100;
-    this.maxHp = 100;
+        this.hp = 100;
+        this.maxHp = 100;
 
-    this.mana = 100;
-    this.hambre = 100;
+        this.mana = 100;
+        this.hambre = 100;
 
-    this.nivel = 1;
-    this.exp = 0;
-
-
-    this.modelo = new THREE.Mesh(
-
-        new THREE.BoxGeometry(
-            0.5,
-            1.2,
-            0.5
-        ),
-
-        new THREE.MeshLambertMaterial({
-            color:0x22c55e
-        })
-
-    );
+        this.nivel = 1;
+        this.exp = 0;
 
 
-    this.modelo.position.y = 0.6;
+        this.modelo = new THREE.Mesh(
+            new THREE.BoxGeometry(
+                0.5,
+                1.2,
+                0.5
+            ),
+            new THREE.MeshLambertMaterial({
+                color:0x22c55e
+            })
+        );
 
-}
+
+        this.modelo.position.y = 0.6;
+
+    }
 
 
 
-agregarAEscena(escena){
+    agregarAEscena(escena){
 
-    escena.add(this.modelo);
+        escena.add(this.modelo);
 
-}
-
-
-
-actualizarPosicion(){
-
-    this.modelo.position.set(
-        this.x,
-        this.y-0.6,
-        this.z
-    );
-
-}
+    }
 
 
 
-actualizarHUD(){
+    actualizarPosicion(){
 
-    const lvl=document.getElementById("lvl");
+        this.modelo.position.set(
+            this.x,
+            this.y-0.6,
+            this.z
+        );
 
-    if(lvl)
-    lvl.textContent=this.nivel;
-
-
-    const pos=document.getElementById("pos");
-
-    if(pos)
-    pos.textContent=
-    `X:${Math.round(this.x)} Z:${Math.round(this.z)}`;
+    }
 
 
-}
 
+    actualizarHUD(){
+
+        let lvl=document.getElementById("lvl");
+
+        if(lvl)
+        lvl.textContent=this.nivel;
+
+
+        let pos=document.getElementById("pos");
+
+        if(pos)
+        pos.textContent=
+        `X:${Math.round(this.x)} Z:${Math.round(this.z)}`;
+
+    }
 
 }
