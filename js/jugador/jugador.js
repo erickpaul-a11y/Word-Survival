7f7ad0feac0abcff9fcb8f5bff57681e95fe368f
erickpaul-a11y <erick.paul@escola.pr.gov.br>
2026-08-01 16:08:09 -0300

    Update jugador.js

--- js/jugador/jugador.js
+++ js/jugador/jugador.js
@@ -1,73 +1,43 @@
+console.log("JUGADOR.JS CARGADO");
+
 class Jugador {
 
-    constructor(){
+constructor(){
 
-        this.x = 0;
-        this.z = 0;
-        this.y = 2;
+    this.x=0;
+    this.z=0;
+    this.y=2;
 
-        this.hp = 100;
-        this.maxHp = 100;
+    this.hp=100;
 
-        this.mana = 100;
-        this.hambre = 100;
+    this.modelo = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5,1.2,0.5),
+        new THREE.MeshLambertMaterial({
+            color:0x22c55e
+        })
+    );
 
-        this.nivel = 1;
-        this.exp = 0;
-
-
-        this.modelo = new THREE.Mesh(
-            new THREE.BoxGeometry(
-                0.5,
-                1.2,
-                0.5
-            ),
-            new THREE.MeshLambertMaterial({
-                color:0x22c55e
-            })
-        );
+}
 
 
-        this.modelo.position.y = 0.6;
+agregarAEscena(escena){
 
-    }
+    escena.add(this.modelo);
 
-
-
-    agregarAEscena(escena){
-
-        escena.add(this.modelo);
-
-    }
+}
 
 
+actualizarPosicion(){
 
-    actualizarPosicion(){
+    this.modelo.position.set(
+        this.x,
+        this.y,
+        this.z
+    );
 
-        this.modelo.position.set(
-            this.x,
-            this.y-0.6,
-            this.z
-        );
-
-    }
-
+}
 
 
-    actualizarHUD(){
-
-        let lvl=document.getElementById("lvl");
-
-        if(lvl)
-        lvl.textContent=this.nivel;
-
-
-        let pos=document.getElementById("pos");
-
-        if(pos)
-        pos.textContent=
-        `X:${Math.round(this.x)} Z:${Math.round(this.z)}`;
-
-    }
+actualizarHUD(){}
 
 }
