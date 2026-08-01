c4e0107c4fba30134023750e930db2a46f852a79
erickpaul-a11y <erick.paul@escola.pr.gov.br>
2026-08-01 13:48:22 -0300

    Update lenguaje.js

--- js/lenguaje/lenguaje.js
+++ js/lenguaje/lenguaje.js
@@ -1,1 +1,20 @@
+function normalizarPalabra(p) {
+    return p.normalize("NFD").replace(/[\u0300-\u036f]/g,"").toLowerCase().trim();
+}
 
+class GestorLenguaje {
+    constructor() {
+        this.letrasConocidas = [];
+        this.configuracionLetras = {};
+    }
+    async cargarDatos() {}
+    puedeEscribir(palabra) {
+        const limpia = normalizarPalabra(palabra).toUpperCase();
+        for (const l of limpia) if (!this.letrasConocidas.includes(l)) return false;
+        return true;
+    }
+    desbloquearLetra(l) { if(!this.letrasConocidas.includes(l.toUpperCase())) this.letrasConocidas.push(l.toUpperCase()); }
+    cargarPartida() {}
+    guardarPartida() {}
+    reiniciar() {}
+}
