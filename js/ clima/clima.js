05dcddaad2bb1a3acfbf6e16ecbde8636e9790d1
erickpaul-a11y <erick.paul@escola.pr.gov.br>
2026-08-01 13:49:26 -0300

    Update clima.js

--- js/ clima/clima.js
+++ js/ clima/clima.js
@@ -1,1 +1,6 @@
-
+class Tiempo {
+    constructor() { this.hora = 8; this.dur = 300; }
+    avanzar(s) { this.hora += s/this.dur*24; if(this.hora>=24)this.hora-=24; }
+    esNoche() { return this.hora<6 || this.hora>19; }
+}
+class Clima { constructor() { this.tipo="soleado"; } actualizar(m){} }
