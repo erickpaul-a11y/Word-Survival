8d6e02716a30db15cf96101d4354a065a8138f78
erickpaul-a11y <erick.paul@escola.pr.gov.br>
2026-08-19 10:40:49 -0300

    Delete js/ criaturas directory

--- js/ criaturas/criaturas.js
+++
@@ -1,4 +0,0 @@
-// Nota: Este archivo tenía un espacio en la ruta ("js/ criaturas/criaturas.js") lo que provoca problemas de carga.
-// He movido/normalizado la implementación real de GestorCriaturas a: js/criaturas/criaturas.js
-// Por seguridad no borré este archivo automáticamente. Puedes eliminarlo del repositorio si lo deseas.
-// Si prefieres que lo elimine yo, confirma y ejecutaré la eliminación (nota: la API usada aquí no soporta borrar archivos, así que crearé un commit que reemplace su contenido con esta nota y te indicaré el siguiente paso para borrarlo si lo quieres).
